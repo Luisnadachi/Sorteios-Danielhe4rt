8b4dea041c831274911de5e9dbbe9ae263e0409c
Nadachi <luiseduardort2001@gmail.com>
2023-02-12 01:33:09 -0300

    projeto iniciado e criado migrations

---
+++ README.md
@@ -0,0 +1,23 @@
+# Sorteios do Danielhe4rt
+
+## Modelagem
+
+```
+table: users
+id: string PK 
+name: string
+email: string 
+password: string
+
+table: courses
+id: int pk
+email: string
+name: string
+modality: string
+link: string
+date_drawn: date
+date_sent: date
+situation: string
+observation: string
+value: integer
+```
---
+++ database/migrations/2023_02_12_025419_create_courses_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->id();
+            $table->string('email');
+            $table->string('name');
+            $table->string('modality');
+            $table->string('link');
+            $table->string('situation');
+            $table->integer('value');
+            $table->date('date_drawn');
+            $table->date('date_sent');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('courses');
+    }
+};
